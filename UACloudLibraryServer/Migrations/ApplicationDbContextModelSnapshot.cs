94055e920ef2eff3f2ab8e90b1ff8a70e9e74a8e
Markus Horstmann <markushorstmann@hotmail.com>
2023-06-20 12:15:31 -0700

    ModelSnapshot: version number

--- UACloudLibraryServer/Migrations/ApplicationDbContextModelSnapshot.cs
+++ UACloudLibraryServer/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -18,7 +18,7 @@ namespace Opc.Ua.Cloud.Library
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.10")
+                .HasAnnotation("ProductVersion", "6.0.16")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
